774c530709b942db3cf38f344844cca53298222c
Alan <lanzn16@outlook.com>
2020-02-16 18:19:54 -0300

    import pages main and login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from './components/Header';
+import Main from './pages/main'
+import Login from './pages/login'
 import './styles.css';
+
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
